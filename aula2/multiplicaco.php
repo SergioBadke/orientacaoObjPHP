7472a95216bb11726d2cb045ef987023c475361e
Sergio Badke <133900607+SergioBadke@users.noreply.github.com>
2024-02-17 15:12:54 -0300

    Update multiplicaco.php

--- aula2/multiplicaco.php
+++ aula2/multiplicaco.php
@@ -1,1 +1,11 @@
-<?php
+<?php 
+
+    $num1=readline("Insira um numero: ");
+    $num2=readline("Insira um numero: ");
+    $resultado=0;
+
+    for($i=0; $i<$num2; $i++){
+
+        $resultado=$resultado+$num1;
+    }
+        print $resultado;
